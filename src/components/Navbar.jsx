cc6b9307d84baf16127e7cd292c95e300d0539d2
AspasijaC <aspasija.cvetkoska@gmail.com>
2023-11-23 17:04:47 +0100

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,41 @@ import avatar from "../data/avatar.jpg";
 import { Cart, Chat, Notifications, UserProfile} from ".";
 import { useStateContext } from '../contexts/ContextProvider';
 
+const NavButton = ({title, customFunc, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position='BottomCenter'>
+    <button type='button' onClick={customFunc} style={ {color} } className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
+      <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'>{icon}</span>
+    </button>
+  </TooltipComponent>
+
+)
+
 const Navbar = () => {
   const {activeMenu, setActiveMenu} = useStateContext();
+  const handleClick = (type) => {
+    // Handle click based on the 'type' parameter (e.g., "cart")
+    console.log(`Clicked on ${type}`);
+  };
   return (
-    <div className='flex justify-between p-2 md:mx-6 relative'></div>
+    <div className='flex justify-between p-2 md:mx-6 relative'>
+      <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu/>}/>
+      <div className='flex'>
+      <NavButton title="Cart" customFunc={() => handleClick("cart")} color="blue" icon={<FiShoppingCart/>}/>
+      <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => handleClick("chat")} color="blue" icon={<BsChatLeft/>}/>
+      <NavButton title="Notifications" dotColor="#03C9D7" customFunc={() => handleClick("notifications")} color="blue" icon={<RiNotification3Line/>}/>
+      <TooltipComponent content="Profile" position='BottomCenter'>
+        <div className='flex item-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg' onClick={() => handleClick("userProfile")}>
+          <img className='rounded-full w-8 h-8' src={avatar} />
+          <p>
+            <span className='text-gray-400 text-14'>Hi, </span>{" "}
+            <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+          </p>
+
+        </div>
+      </TooltipComponent>
+
+      </div>
+    </div>
   )
 }
 
