6c71d35aa8132cee349b45b7ac382643403f450d
AspasijaC <aspasija.cvetkoska@gmail.com>
2023-11-23 19:47:45 +0100

    finished

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -81,42 +81,31 @@ const gridEmployeeCountry = (props) => (
 export const EditorData = () => (
   <div>
     <h3>
-      Try React
-      React has been designed from the start for gradual adoption, and you can use as little or as much React as you need. Whether you want to get a taste of React, add some interactivity to a simple HTML page, or start a complex React-powered app, the links in this section will help you get started.
+      In the vast tapestry of existence, every individual thread weaves a unique story. From the quiet moments of self-discovery to the grand adventures that define our path, life unfolds with both challenges and triumphs. It's a journey of continual growth and evolution.
 
-      Online Playgrounds
-      If you’re interested in playing around with React, you can use an online code playground. Try a Hello World template on CodePen, CodeSandbox, or Stackblitz.
+      As the seasons change, so do the chapters of our lives. We encounter diverse landscapes of experience, from the exhilarating highs to the profound lows. Amidst the complexity, there lies a beauty in the simplicity of shared laughter, the warmth of genuine connections, and the solace found in quiet reflection.
 
-      If you prefer to use your own text editor, you can also download this HTML file, edit it, and open it from the local filesystem in your browser. It does a slow runtime code transformation, so we’d only recommend using this for simple demos.
+      In the tapestry of human connection, empathy is the golden thread that binds us together. It enables us to understand each other's joys and sorrows, fostering a sense of unity in our shared human experience. Through empathy, we discover the strength to overcome adversity and the capacity to celebrate one another's successes.
 
-      Add React to a Website
-      You can add React to an HTML page in one minute. You can then either gradually expand its presence, or keep it contained to a few dynamic widgets.
-
-      Create a New React App
-      When starting a React project, a simple HTML page with script tags might still be the best option. It only takes a minute to set up!
-
+      The passage of time, marked by the ticking of seconds and the turning of years, brings with it both nostalgia for moments past and anticipation for what lies ahead. It teaches us the importance of resilience, adaptability, and the art of embracing change.
       As your application grows, you might want to consider a more integrated setup. There are several JavaScript toolchains we recommend for larger applications. Each of them can work with little to no configuration and lets you take full advantage of the rich React ecosystem. Learn how.
 
-      Learn React
-      People come to React from different backgrounds and with different learning styles. Whether you prefer a more theoretical or a practical approach, we hope you’ll find this section helpful.
+      In this intricate dance of existence, we find meaning in our pursuits, purpose in our relationships, and wisdom in our reflections. Life's canvas is painted with the brushstrokes of our choices, passions, and the indelible mark we leave on the world.
 
-      If you prefer to learn by doing, start with our practical tutorial.
-      If you prefer to learn concepts step by step, start with our guide to main concepts.
-      Like any unfamiliar technology, React does have a learning curve. With practice and some patience, you will get the hang of it.
+      So, as we navigate the vast expanse of our lives, let us savor the richness of each chapter, find strength in our vulnerabilities, and cultivate a spirit of gratitude for the tapestry that is uniquely ours.
 
-      First Examples
-      The React homepage contains a few small React examples with a live editor. Even if you don’t know anything about React yet, try changing their code and see how it affects the result.
+      In the vast tapestry of existence, every individual thread weaves a unique story. From the quiet moments of self-discovery to the grand adventures that define our path, life unfolds with both challenges and triumphs. It's a journey of continual growth and evolution.
 
-      React for Beginners
-      If you feel that the React documentation goes at a faster pace than you’re comfortable with, check out this overview of React by Tania Rascia. It introduces the most important React concepts in a detailed, beginner-friendly way. Once you’re done, give the documentation another try!
+      As the seasons change, so do the chapters of our lives. We encounter diverse landscapes of experience, from the exhilarating highs to the profound lows. Amidst the complexity, there lies a beauty in the simplicity of shared laughter, the warmth of genuine connections, and the solace found in quiet reflection.
 
-      React for Designers
-      If you’re coming from a design background, these resources are a great place to get started.
+      In the tapestry of human connection, empathy is the golden thread that binds us together. It enables us to understand each other's joys and sorrows, fostering a sense of unity in our shared human experience. Through empathy, we discover the strength to overcome adversity and the capacity to celebrate one another's successes.
 
-      JavaScript Resources
-      The React documentation assumes some familiarity with programming in the JavaScript language. You don’t have to be an expert, but it’s harder to learn both React and JavaScript at the same time.
+      The passage of time, marked by the ticking of seconds and the turning of years, brings with it both nostalgia for moments past and anticipation for what lies ahead. It teaches us the importance of resilience, adaptability, and the art of embracing change.
+      As your application grows, you might want to consider a more integrated setup. There are several JavaScript toolchains we recommend for larger applications. Each of them can work with little to no configuration and lets you take full advantage of the rich React ecosystem. Learn how.
 
-      We recommend going through this JavaScript overview to check your knowledge level. It will take you between 30 minutes and an hour but you will feel more confident learning React.
+      In this intricate dance of existence, we find meaning in our pursuits, purpose in our relationships, and wisdom in our reflections. Life's canvas is painted with the brushstrokes of our choices, passions, and the indelible mark we leave on the world.
+
+      So, as we navigate the vast expanse of our lives, let us savor the richness of each chapter, find strength in our vulnerabilities, and cultivate a spirit of gratitude for the tapestry that is uniquely ours.
     </h3>
   </div>
 );
